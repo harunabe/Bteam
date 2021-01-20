49ee2f5bb99f2d51a1ee37617752793c1e85d04b
y20516 <77314437+y20516@users.noreply.github.com>
2021-01-20 14:52:05 +0900

    時間制限つけました
    つけました

--- puzzle.js
+++ puzzle.js
@@ -17,6 +17,35 @@ $(function() {
     }).appendTo($('body'));
   }
 
+  setTimeout(function(){
+    window.location.href = 'miss.html';},30 * 1000); //時間制限
+
+  //Timer
+  const totalTime = 30000;  //時間表示
+  const oldTime = Date.now();
+
+  const timeId = setInterval(() => {
+    const currentTime = Date.now();
+
+    // 差分を求める
+    const diff = currentTime - oldTime;
+
+    const diffSec = totalTime - diff;
+
+    //ミリ秒を整数に変換
+    const remainSec = Math.ceil(diffSec / 1000);
+
+    let text = `残り${remainSec}秒`;
+
+    // 0秒以下になったら
+    if (diffSec <= 0) {
+    clearInterval(timeId);
+  }
+
+  // 画面に表示する
+  document.querySelector('#log').innerHTML = text;
+})
+
   // ピースをクリックしたとき
   $('.piece').click(function() {
     var no = this.id.substring(5);
@@ -53,3 +82,4 @@ function swap(i, j) {
   pos[i] = pos[j];
   pos[j] = tmp;
 }
+
